a088aa40600f1700a0b881ec031c80eb82f3231a
henriquesoto92 <henriquesoto92@gmail.com>
2023-05-21 17:10:03 -0300

    modificando o Avatar

--- src/app/components/SideBar/Avatar/index.tsx
+++ src/app/components/SideBar/Avatar/index.tsx
@@ -3,13 +3,13 @@ import React from "react";
 
 const Avatar: React.FC = () => {
   return (
-    <div className="rounded-full w-25 h-25 border shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] flex overflow-hidden">
+    <div className="rounded-full w-25 h-25 border-8 border-sky-800 shadow-[0_0px_20px_0px_rgba(0,0,0,0.3)] overflow-hidden">
       <Image
         src="/assets/icons/iconProfile.svg"
-        height={150}
+        height={160}
         width={150}
         alt="Image Profile"
-        className="drop-shadow-lg"
+        className="drop-shadow-lg bg-white"
       />
     </div>
   );
